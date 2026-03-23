8f45d2ee0fd3e5146a469ef9971f4eef5062c566
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-23 20:56:31 +0200

    Add project files.

---
+++ WebMVC9DBFirstPro2/Models/Capability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebMVC9DBFirstPro2.Models;
+
+public partial class Capability
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public virtual ICollection<Role> Roles { get; set; } = new List<Role>();
+}
